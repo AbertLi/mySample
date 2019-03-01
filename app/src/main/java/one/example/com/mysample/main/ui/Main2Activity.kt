0254d35e301d3a892d0046766db84960143a55b9
jiAlbert_Lee_company_eclipse <3185017028@qq.com>
2019-03-01 19:18:01 +0800

    加入Kotlin的语言。

---
+++ app/src/main/java/one/example/com/mysample/main/ui/Main2Activity.kt
@@ -0,0 +1,13 @@
+package one.example.com.mysample.main.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import one.example.com.mysample.R
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
